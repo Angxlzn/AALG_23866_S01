88a8b3d3ee09551557af9db1aba6b4874158015b
Angxlzn <miguelangelestombelo@gmail.com>
2026-08-17 21:06:47 -0500

    codigo de python basico

---
+++ Program.py
@@ -0,0 +1,13 @@
+#C#
+# Console.WriteLine("Cual es tu nombre?");
+# String nom = Console.ReadLine();
+# Console.WriteLine(Cual es tu nombre?");
+# int edad = convert.ToInt32(Console.ReadLine());
+# Console.WriteLine($"Hola {nom}, tienes {edad} años");
+
+print ("Cual es tu nombre?")
+nom = input()
+print (f"Hola {nom}")
+print ("Cual es tu edad?")
+edad = int(input())
+print (f"Hola {nom}, tienes {edad} años")
